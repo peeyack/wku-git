054d7906cb349e0349b839697ae0513221c31d8d
wku <wkusmierczuk@wp.pl>
2017-02-11 14:16:11 +0100

    Wystrzeliwanie rakiet w CCCP

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,5 +3,10 @@ public class SomeProgram {
         System.out.println("We are learning to use Git.");
 		System.out.println("Git is crappy software");
 		System.out.println("Git is terrible");
+	public void manageTheRockets() {
+    // TODO implement
+	System.out.println("Launch all rockets into the Soviet Union");
+}
+
 	}
 }
