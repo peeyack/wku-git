72992fed71336201ac489296edfd78262d522ca8
wku <wkusmierczuk@wp.pl>
2017-02-11 14:04:40 +0100

    Dodano opcje wystrzeliwania rakiet

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,5 +3,16 @@ public class SomeProgram {
         System.out.println("We are learning to use Git.");
 		System.out.println("Git is crappy software");
 		System.out.println("Git is terrible");
+	<<<<<<< HEAD
+    public void manageRockets() {
+        // TODO implement
+    }
+=======
+    public void manageTheSatellite() {
+        // TODO implement
+    }
+>>>>>>> managing-satellite
+}
+
 	}
 }
